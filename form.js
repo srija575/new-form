67f40bc9da52c01cb9dc3395facadb6798ef84a1
srija575 <ksrija41@gmail.com>
2024-09-09 09:24:40 +0600

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+function demo(){
+    if(document.getElementById("us").value== 'entri elevate' && document.getElementById("pw").value== 'admin123'){
+    window.location.href=("https://srija575.github.io/Js-calculator/")
+    alert("Login successful");
+    }
+    else{
+        alert("Enter valid username and password");
+        return;
+    }
+}
